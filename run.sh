996b941b5d666197ed18bf0026c6b292e854226e
Thong Nguyen <thongnguyen2410@yahoo.com>
2019-10-14 22:15:27 -0500

    add InMapperWordCount

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DIR=part1/c/
-FILE=WordCount
+if [[ $# -lt 2 ]]; then
+	echo "Usage  : $0 <package_dir> <class_name>"
+	echo "Example: $0 part1/c WordCount"
+        exit 1
+fi;
+
+DIR=$1/
+FILE=$2
 CP="./bin:/usr/lib/hadoop/*:/usr/lib/hadoop/lib/*:/usr/lib/hadoop/client-0.20/*"
 
 rm -f bin/${DIR}*.class  bin/${FILE}.jar
@@ -9,16 +15,20 @@ rm -f bin/${DIR}*.class  bin/${FILE}.jar
 javac -cp $CP src/${DIR}${FILE}.java -d bin
 cd bin;jar cfv ${FILE}.jar ${DIR}*.class;cd -
 
-if [ "$1" == "local" ]; then
+if [ "$3" == "local" ]; then
 	java -cp $CP ${DIR}${FILE} input/${DIR} output
 	cat output/*
 	exit 0
 fi;
 
-HDIR=/usr/cloudera
+HDIR=/user/cloudera
 
+hadoop fs -rm ${HDIR}/input/*
+hadoop fs -rmdir ${HDIR}/input
 hadoop fs -mkdir ${HDIR}/input
 hadoop fs -copyFromLocal input/${DIR}*.txt ${HDIR}/input
 hadoop jar bin/${FILE}.jar ${DIR}${FILE} ${HDIR}/input ${HDIR}/output
+echo "=================================================="
+echo "hadoop fs -cat ${HDIR}/output/*"
 echo "=================================================="
 hadoop fs -cat ${HDIR}/output/*
---
+++ src/part1/d/InMapperWordCount.java
@@ -0,0 +1,93 @@
+package part1.d;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class InMapperWordCount {
+
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		//private final static IntWritable one = new IntWritable(1);
+		//private Text word = new Text();
+		private HashMap<Text, Integer> H;
+		
+		@Override
+		public void setup(Context context) {
+			H = new HashMap<>();
+		}
+
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				Text word = new Text(tokenizer.nextToken());
+				//context.write(word, one);
+				if (H.containsKey(word)) {
+					H.put(word, H.get(word) + 1);
+				} else {
+					H.put(word, 1);
+				}
+			}
+		}
+		
+		@Override
+		public void cleanup(Context context) throws IOException, InterruptedException {
+			for (Text key: H.keySet()) {
+				context.write(key, new IntWritable(H.get(key)));
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		@SuppressWarnings("deprecation")
+		Job job = new Job(conf, "wordcount");
+		job.setJarByClass(InMapperWordCount.class);
+
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		Path output = new Path(args[1]);
+		// configuration should contain reference to your namenode
+		FileSystem fs = FileSystem.get(conf);		
+		if (fs.exists(output)) {
+			// true stands for recursively deleting the folder you gave
+			fs.delete(output, true);
+		}
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, output);
+
+		job.waitForCompletion(true);
+	}
+
+}
